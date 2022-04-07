54207a5308acd328ff604c7f125fa6745b5444a2
chanmin, kim <kimchanmin1@gmail.com>
2022-04-07 17:24:48 +0900

    feat: implement signup page

--- sg-client/src/index.js
+++ sg-client/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainPage from './pages/Main';
 import SignInPage from './pages/SignIn';
-import SignUpPage from 'pages/Signup';
+import SignUpPage from './pages/SignUp';
+import NotFoundPage from 'pages/404';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import "./styles/common.css"
 import "./styles/default.css"
 import "./styles/admin.css"
-import "./styles/login.css"
+import "./styles/SignIn.css"
+// import "./styles/SignUp.css"
 
 ReactDOM.render(
   <div className="wrap fold_lnb">
@@ -16,6 +18,7 @@ ReactDOM.render(
         <Route path="/admin" element={<MainPage />} />
         <Route path="/signIn" element={<SignInPage />} />
         <Route path="/signUp" element={<SignUpPage />} />
+        <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
     </Router>
   </div>,
---
+++ sg-client/src/pages/404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>페이지를 찾을 수 없습니다.</div>
+  )
+}
+
+export default NotFoundPage
