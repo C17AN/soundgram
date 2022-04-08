50330b9b1f594ff63f89a1f1fcfb5a5048ffffa1
chanmin, kim <kimchanmin1@gmail.com>
2022-04-08 15:01:49 +0900

    style: apply module css

--- sg-client/src/components/pages/SignIn/index.jsx
+++ sg-client/src/components/pages/SignIn/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import loginLogo from "assets/images/login_logo.png"
-import style from "styles/pages/SignIn.module.css"
+import signInStyle from "styles/pages/SignIn.module.css"
 import defaultStyle from "styles/default.module.css"
+import commonStyle from "styles/common.module.css"
 import { Link, Navigate, useNavigate } from 'react-router-dom'
 import "styles/bootstrap.css"
 
@@ -13,49 +14,49 @@ const SignIn = () => {
   }
 
   return (
-    <div className={style.login_wrap}>
-      <div className={style.left}>
-        <div className={style.logo}>
+    <div className={signInStyle.login_wrap}>
+      <div className={signInStyle.left}>
+        <div className={signInStyle.logo}>
           <img src={loginLogo} alt="BrandCast" />
         </div>
         <span>ver. 2021S3</span>
         <Link to="/signUp">
-          <div className={style.btn_wrap}>
+          <div className={signInStyle.btn_wrap}>
             <button type="button">회원가입</button>
           </div>
         </Link>
       </div>
-      <div className={style.right}>
-        <div className={style["form-group"]}>
+      <div className={signInStyle.right}>
+        <div className={signInStyle["form-group"]}>
           <label for="login_id" className={defaultStyle.blind}></label>
           <input
-            className={style["form-control"]}
+            className={signInStyle["form-control"]}
             type="text"
             id="login_id"
             name="id"
             placeholder="아이디"
           />
         </div>
-        <div className={style["form-group"]}>
+        <div className={signInStyle["form-group"]}>
           <label for="login_pw" className={defaultStyle.blind}></label>
           <input
-            className={style["form-control"]}
+            className={signInStyle["form-control"]}
             type="password"
             id="login_pw"
             name="pwd"
             placeholder="패스워드"
           />
         </div>
-        <div className={`${style["form-group"]} ${style["line_txt"]}`}>
-          <span className={style.save}>
+        <div className={`${signInStyle["form-group"]} ${signInStyle["line_txt"]}`}>
+          <span className={signInStyle.save}>
             <input type="checkbox" class="" name="ming" id="ming" />
             <label class="" for="ming">아이디 저장</label>
           </span>
-          <span className={style.such}>
+          <span className={signInStyle.such}>
             <a href="#">아이디 찾기 / 임시 비밀번호</a>
           </span>
         </div>
-        <div className={style["form-group"]}>
+        <div className={signInStyle["form-group"]}>
           <button class="" type="submit" onClick={onSignIn}>로그인</button>
         </div>
       </div>
--- sg-client/src/layouts/SignInLayout.jsx
+++ sg-client/src/layouts/SignInLayout.jsx
@@ -1,13 +1,16 @@
 import Footer from 'components/common/Footer'
 import React from 'react'
 import style from "styles/pages/SignIn.module.css"
+import commonStyle from "styles/common.module.css"
 
 const SignInLayout = ({ children }) => {
   return (
     <>
       <div className={style.deco_line} />
-      <div className={style.contents_wrap}>
-        {children}
+      <div className={`${commonStyle.wrap} ${commonStyle.fold_lnb}`}>
+        <div className={style.contents_wrap}>
+          {children}
+        </div>
       </div>
       <Footer />
     </>
