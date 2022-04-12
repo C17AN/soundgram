b6076c305e7206b936c1d2170780a4ae11b06453
chanmin, kim <kimchanmin1@gmail.com>
2022-04-12 17:26:50 +0900

    feat: add classname package

--- sg-client/src/layouts/DefaultLayout.jsx
+++ sg-client/src/layouts/DefaultLayout.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import Header from 'components/common/Header'
 import Sidebar from 'components/common/Sidebar'
 import Footer from 'components/common/Footer'
+import useIsMobile from 'hooks/useIsMobile'
+import commonStyle from "styles/common.module.css"
+import classNames from 'classnames'
 
 const DefaultLayout = ({ children }) => {
+  const isMobile = useIsMobile()
   return (
     <>
-      <div className="wrap fold_lnb">
-        <Header />
-        <Sidebar />
+      <div className={classNames("wrap", { [commonStyle["fold_lnb"]]: isMobile })}>
+        <div className={commonStyle.contents_wrap} id="contents_wrap">
+          <Header />
+          <Sidebar />
+          <>{children}</>
+        </div>
+        <Footer />
       </div>
-      <div className="contents_wrap">
-        {children}
-      </div>
-      <Footer />
     </>
   )
 }
